a48e886c812a66c0d4d6178947cc07244cf69c70
Nik Ho <studying.mole@gmail.com>
2024-08-21 17:39:13 +1200

    move 4.27 files to root level

---
+++ Plugins/BLUI/Source/Blu/Private/Blu.cpp
@@ -0,0 +1,61 @@
+#include "IBlu.h"
+#include "BluManager.h"
+
+class FBlu : public IBlu
+{
+
+	/** IModuleInterface implementation */
+	virtual void StartupModule() override
+	{
+		CefString GameDirCef = *FPaths::ConvertRelativePathToFull(FPaths::ProjectDir() + "BluCache");
+		FString ExecutablePath = FPaths::ConvertRelativePathToFull(FPaths::ProjectDir() + "Plugins/BLUI/ThirdParty/cef/");
+		CefString CefLogPath = *FPaths::ConvertRelativePathToFull(FPaths::ProjectDir() + "Saved/Logs/blui_cef.log");
+
+		// Setup the default settings for BluManager
+		BluManager::Settings.windowless_rendering_enabled = true;
+		BluManager::Settings.no_sandbox = true;
+		BluManager::Settings.uncaught_exception_stack_size = 5;
+		BluManager::Settings.multi_threaded_message_loop = false;
+		BluManager::Settings.log_severity = LOGSEVERITY_ERROR;
+
+	#if PLATFORM_LINUX
+		ExecutablePath = "./blu_ue4_process";
+	#endif
+	#if PLATFORM_MAC
+		ExecutablePath += "Mac/shipping/blu_ue4_process.app/Contents/MacOS/blu_ue4_process";
+	#endif
+	#if PLATFORM_WINDOWS
+		ExecutablePath += "Win/shipping/blu_ue4_process.exe";
+	#endif
+
+		CefString realExePath = *ExecutablePath;
+
+		// Set the sub-process path
+		CefString(&BluManager::Settings.browser_subprocess_path).FromString(realExePath);
+		// Set the cache path
+		CefString(&BluManager::Settings.cache_path).FromString(GameDirCef);
+		// Cef logs
+		CefString(&BluManager::Settings.log_file).FromString(CefLogPath);
+
+		// Make a new manager instance
+		CefRefPtr<BluManager> BluApp = new BluManager();
+
+		//CefExecuteProcess(BluManager::main_args, BluApp, NULL);
+		CefInitialize(BluManager::MainArgs, BluManager::Settings, BluApp, NULL);
+
+		UE_LOG(LogBlu, Log, TEXT(" STATUS: Loaded"));
+	}
+
+	virtual void ShutdownModule() override
+	{
+		UE_LOG(LogBlu, Log, TEXT(" STATUS: Shutdown"));
+		CefShutdown();
+	}
+
+};
+
+
+
+
+IMPLEMENT_MODULE( FBlu, Blu )
+DEFINE_LOG_CATEGORY(LogBlu);

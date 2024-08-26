068ae72f2a42ec60c3aa9a05e369af6a34e09297
Nik Ho <studying.mole@gmail.com>
2024-08-27 00:22:17 +1200

    replace 4.27 assets, convert 4.27 project to 4.26

---
+++ Source/SampleGame426/SampleGame426.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SampleGame426.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SampleGame426, "SampleGame426" );
---
+++ Source/SampleGame426/SampleGame426GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SampleGame426GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SAMPLEGAME426_API ASampleGame426GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};

e182dd5c363dfad8ef3992a4de5ebff186e6b63e
Jeffrey <jeffrey.wong@immutable.com>
2024-10-24 13:23:28 +1300

    chore: applied code formatting for AWStackTopControlPanel (#3375)

--- Source/SampleGame426/Public/UI/Base/AWStackTopControlPanel.h
+++ Source/SampleGame426/Public/UI/Base/AWStackTopControlPanel.h
@@ -1,13 +1,15 @@
 ﻿#pragma once
 
 #include "CustomUserWidget.h"
-#include "TopPanelButton.h"
 
 #include "AWStackTopControlPanel.generated.h"
 
+class UHorizontalBox;
+class UTopPanelButton;
 
 /**
- * 
+ * @class UAWStackTopControlPanel  
+ * @brief User widget with main and secondary button functionality
  */
 UCLASS(Abstract)
 class SAMPLEGAME426_API UAWStackTopControlPanel : public UCustomUserWidget
@@ -15,21 +17,25 @@ class SAMPLEGAME426_API UAWStackTopControlPanel : public UCustomUserWidget
 	GENERATED_BODY()
 
 public:
+	UTopPanelButton* GetMainButton(UTopPanelButton* SecondaryButton);
+
 	UTopPanelButton* AddMainButton(const FString& ButtonTitle, int32 Index);
 	UTopPanelButton* AddSecondaryButton(UTopPanelButton* MainButton, const FString& ButtonTitle, int32 Index);
+
 	void ShowSecondaryButtons(UTopPanelButton* MainButton);
-	UTopPanelButton* GetMainButton(UTopPanelButton* SecondaryButton);
 
 protected:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Buttons")
 	TSoftClassPtr<UTopPanelButton> MainButtonClass;
+
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Buttons")
 	TSoftClassPtr<UTopPanelButton> SecondaryButtonClass;
 
-	UPROPERTY(BlueprintReadOnly, meta=(BindWidget))
-	class UHorizontalBox* MainButtonPanel = nullptr;
-	UPROPERTY(BlueprintReadOnly, meta=(BindWidget))
-	class UHorizontalBox* SecondaryButtonPanel = nullptr;
+	UPROPERTY(BlueprintReadOnly, Meta = (BindWidget))
+	UHorizontalBox* MainButtonPanel = nullptr;
+
+	UPROPERTY(BlueprintReadOnly, Meta = (BindWidget))
+	UHorizontalBox* SecondaryButtonPanel = nullptr;
 
 private:
 	TMap<UTopPanelButton* /* Main Button */, TArray<UTopPanelButton*> /* Secondary Buttons */> MapMainToSecondaryButtons;

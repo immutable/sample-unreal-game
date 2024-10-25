7f05778163b94033124ade4030afd99394163963
Jeffrey <jeffrey.wong@immutable.com>
2024-10-25 17:43:22 +1300

    fix: start button and game loop (#3366)

---
+++ Source/SampleGame426/Public/GameFramework/CustomGameMode.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "GameUIPolicy.h"
+
+#include "CustomGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SAMPLEGAME426_API ACustomGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void InitializeHUDForPlayer_Implementation(APlayerController* NewPlayer) override;
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Classes", Meta = (DisplayName = "UI Policy Class"))
+	TSoftClassPtr<UGameUIPolicy> UIPolicyClass;
+};

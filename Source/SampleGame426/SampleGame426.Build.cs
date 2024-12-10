e8e2f702607b2d6f560de5bbcd81e2e94701d42b
Yermek Garifullanov <yermek.garifullanov@immutable.com>
2024-12-11 10:04:37 +1300

    feat: functional transak widget

--- Source/SampleGame426/SampleGame426.Build.cs
+++ Source/SampleGame426/SampleGame426.Build.cs
@@ -19,6 +19,7 @@ public class SampleGame426 : ModuleRules
 			"CoreUObject", 
 			"Engine", 
 			"InputCore",
+			"WebBrowserWidget",
 			"Immutable",
 			"ImmutableOrderbook", 
 			"ImmutablezkEVMAPI",

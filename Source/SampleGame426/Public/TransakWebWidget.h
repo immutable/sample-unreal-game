d086279eb9c106e67d7f137af7b980c852c91aff
Yermek Garifullanov <yermek.garifullanov@immutable.com>
2024-12-11 10:04:37 +1300

    feat: functional transak widget

---
+++ Source/SampleGame426/Public/TransakWebWidget.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+#include "WebBrowser.h"
+#include "Blueprint/UserWidget.h"
+
+#include "TransakWebWidget.generated.h"
+
+
+/**
+ * 
+ */
+UCLASS(Abstract)
+class SAMPLEGAME426_API UTransakWebWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	void NativeOnInitialized() override;
+	
+	UFUNCTION(BlueprintCallable)
+	void Load(const FString& WalletAddress, const FString& Email, const FString& ProductsAvailed, const FString& ScreenTitle);
+	
+private:
+	FString ComputePath(const FString& WalletAddress, const FString& Email, const FString& ProductsAvailed, const FString& ScreenTitle);
+	UFUNCTION()
+	void OnConsoleMessage(const FString& Message, const FString& Source, int32 Line);
+	UFUNCTION()
+	void OnUrlChanged(const FText& Text);
+	void LoadURL();
+
+protected:
+	UPROPERTY(BlueprintReadOnly, Category = "Web", meta=(BindWidget))
+	UWebBrowser* WebBrowserWidget = nullptr;
+
+private:
+	FString UrlToLoad;
+};
